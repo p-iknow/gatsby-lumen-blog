367416b9d1e7085f442cfc4c731f5f74b1e2acf1
P-iknow <apricotsoul@gmail.com>
2019-06-04 21:43:41 +0900

    fix: til 카테고리 article 에서 조회되지 않도록 수정

--- src/templates/index-template.js
+++ src/templates/index-template.js
@@ -11,7 +11,7 @@ import type { PageContext, AllMarkdownRemark } from '../types';
 
 type Props = {
   data: AllMarkdownRemark,
-  pageContext: PageContext
+  pageContext: PageContext,
 };
 
 const IndexTemplate = ({ data, pageContext }: Props) => {
@@ -22,12 +22,12 @@ const IndexTemplate = ({ data, pageContext }: Props) => {
     hasNextPage,
     hasPrevPage,
     prevPagePath,
-    nextPagePath
+    nextPagePath,
   } = pageContext;
 
-
   const { edges } = data.allMarkdownRemark;
-  const pageTitle = currentPage > 0 ? `Posts - Page ${currentPage} - ${siteTitle}` : siteTitle;
+  const pageTitle =
+    currentPage > 0 ? `Posts - Page ${currentPage} - ${siteTitle}` : siteTitle;
 
   return (
     <Layout title={pageTitle} description={siteSubtitle}>
@@ -45,14 +45,21 @@ const IndexTemplate = ({ data, pageContext }: Props) => {
   );
 };
 
+// category: { ne: "til" } til 카테고리가 article 영역에 추가되지 않도록 조정
 export const query = graphql`
   query IndexTemplate($postsLimit: Int!, $postsOffset: Int!) {
     allMarkdownRemark(
-        limit: $postsLimit,
-        skip: $postsOffset,
-        filter: { frontmatter: { template: { eq: "post" }, draft: { ne: true } } },
-        sort: { order: DESC, fields: [frontmatter___date] }
-      ){
+      limit: $postsLimit
+      skip: $postsOffset
+      filter: {
+        frontmatter: {
+          template: { eq: "post" }
+          draft: { ne: true }
+          category: { ne: "til" }
+        }
+      }
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
       edges {
         node {
           fields {
