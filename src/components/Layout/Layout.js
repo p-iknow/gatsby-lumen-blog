d5288c8848c62648a70babed16e4d6984a153205
P-iknow <apricotsoul@gmail.com>
2020-02-08 22:40:51 +0900

    feat:  meta data 추가

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -8,18 +8,27 @@ type Props = {
   children: ReactNode,
   title: string,
   description?: string,
+  keywords?: Array,
+  author?: string,
 };
 
-const Layout = ({ children, title, description }: Props) => (
+const Layout = ({ children, title, description, keywords, author }: Props) => (
   <div className={styles.layout}>
     <Helmet>
       <html lang="ko" />
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta name="keywords" content={keywords && keywords.join(', ')} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content="website" />
       <meta property="og:site_name" content={title} />
+      <meta property="og:image" content />
       <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content={author} />
       <meta name="twitter:title" content={title} />
-      {/* 폰트 등록 */}
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content />
     </Helmet>
     {children}
   </div>
